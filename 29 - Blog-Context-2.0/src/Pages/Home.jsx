6e6b3b510c8691ca9b49656870614a1a2adf51c5
Ajay Kumar <126579063+AjayKumar0786@users.noreply.github.com>
2024-09-11 22:25:56 +0530

    Add files via upload

---
+++ 29 - Blog-Context-2.0/src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Blogs from '../Components/Blogs'
+import Pagination from '../Components/Pagination'
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <Header />
+            <Blogs />
+            <Pagination />
+            
+        </div> 
+    )
+}
+
+export default Home
